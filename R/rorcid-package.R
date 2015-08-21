250a262c2a63657f84da9a4b0e162d4adc0d7eb0
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-21 00:17:07 -0700

    import non-base R pkgs, require httr > 1, bump 99 version

--- R/rorcid-package.R
+++ R/rorcid-package.R
@@ -1,5 +1,8 @@
 #' A programmatic R interface the Orcid.org API.
 #' 
+#' @importFrom methods is
+#' @importFrom stats setNames
+#' @importFrom utils browseURL head
 #' @importFrom httr GET content stop_for_status accept
 #' @importFrom jsonlite fromJSON
 #' @name rorcid-package
