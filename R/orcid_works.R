00dbd05cf7c8ad09dac1b5600191cea4c388b820
Scott Chamberlain <myrmecocystus@gmail.com>
2018-03-30 18:24:49 -0700

    #44 another attempt at harmonizing output of orcid_works output

--- R/orcid_works.R
+++ R/orcid_works.R
@@ -17,12 +17,17 @@
 #' # get all works
 #' res <- orcid_works(orcid = "0000-0002-9341-7985")
 #' res$`0000-0002-9341-7985`
-#' res$`0000-0002-9341-7985`$type
+#' res$`0000-0002-9341-7985`$works
+#' res$`0000-0002-9341-7985`$works$type
 #' str(res$`0000-0002-9341-7985`)
 #' 
 #' # get individual works
-#' orcid_works(orcid = "0000-0002-9341-7985", put_code = 5011717)
-#' orcid_works(orcid = "0000-0002-9341-7985", put_code = c(5011717, 15536016))
+#' a <- orcid_works(orcid = "0000-0002-9341-7985", put_code = 5011717)
+#' a$`0000-0002-9341-7985`
+#' a$`0000-0002-9341-7985`$works
+#' b <- orcid_works(orcid = "0000-0002-9341-7985", 
+#'    put_code = c(5011717, 15536016))
+#' b$`0000-0002-9341-7985`
 #' 
 #' # change formats
 #' orcid_works("0000-0002-9341-7985", 5011717, "application/json")
@@ -35,15 +40,16 @@
 #' # get citations
 #' id <- "0000-0001-7678-8656"
 #' x <- orcid_works(id)
-#' orcid_works(id, put_code = x[[1]]$`put-code`)[[1]]$`work.citation.citation-value`
+#' wks <- orcid_works(id, put_code = x[[1]]$works$`put-code`)
+#' wks[[1]]$works$`work.citation.citation-value`
 #' 
 #' ## or send many put codes at once, will be split into chunks of 50 each
 #' id <- "0000-0001-6758-5101"
-#' x <- orcid_works(id)
-#' pcodes <- x[[1]]$`put-code`
+#' z <- orcid_works(id)
+#' pcodes <- z[[1]]$works$`put-code`
 #' length(pcodes)
 #' res <- orcid_works(orcid = id, put_code = pcodes)
-#' lapply(res$`0000-0001-6758-5101`, head, n = 1)
+#' head(res$`0000-0001-6758-5101`$works)
 #' }
 orcid_works <- function(orcid, put_code = NULL, format = "application/json", 
   ...) {
@@ -61,6 +67,7 @@ orcid_works <- function(orcid, put_code = NULL, format = "application/json",
       chunks <- split(put_code, ceiling(seq_along(put_code) / 50))
       lapply(chunks, function(z) file.path("works", paste0(z, collapse = ",")))
     } else {
+      # list(file.path("work", put_code))
       file.path("work", put_code)
     }
   }
@@ -68,27 +75,28 @@ orcid_works <- function(orcid, put_code = NULL, format = "application/json",
   if (length(pth) > 1) {
     tmp <- Map(function(z) orcid_prof_helper(orcid, z, ctype = format), pth)
     tt <- unname(lapply(tmp, function(z) z$bulk))
-    stats::setNames(list(tt), orcid)
+    tt <- as_dt(tt, FALSE)
+    stats::setNames(list(list(works = tt)), orcid)
   } else {
     tmp <- lapply(orcid, orcid_prof_helper, path = pth, ctype = format, ...)
     tt <- lapply(tmp, function(z) {
       if (grepl("works", pth) || (is.list(pth) && grepl("works/", pth[[1]]))) {
         if (is.null(put_code)) {
           bb <- z$group$`work-summary`
-          if (is_json(format)) list(as_dt(bb, FALSE)) else list(bb)
+          if (is_json(format)) list(works = as_dt(bb, FALSE)) else list(works = bb)
         } else {
-          list(z$bulk)
+          list(works = z$bulk)
         }
       } else if (grepl("work/", pth)) {
         if (is_json(format)) {
-          jsonlite::fromJSON(
+          list(works = jsonlite::fromJSON(
             jsonlite::toJSON(list(z, z), auto_unbox=TRUE)
-          )[1,]
+          )[1,])
         } else {
-          z
+          list(works = z)
         }
       } else {
-        z$bulk
+        list(works = z$bulk)
       }
     })
     stats::setNames(tt, orcid)
