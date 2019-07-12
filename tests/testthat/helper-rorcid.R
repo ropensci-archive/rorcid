96dd2b710ba1f6296805475e1031ce789cb30d0d
Scott Chamberlain <myrmecocystus@gmail.com>
2019-07-12 14:31:30 +0200

    remove internet helper methods in helper test file

--- tests/testthat/helper-rorcid.R
+++ tests/testthat/helper-rorcid.R
@@ -4,16 +4,3 @@ invisible(vcr::vcr_configure(
     dir = "../fixtures",
     filter_sensitive_data = list("<<rorcid_bearer_token>>" = Sys.getenv('ORCID_TOKEN'))
 ))
-
-has_internet <- function() {
-  z <- try(suppressWarnings(readLines('https://www.google.com', n = 1)), 
-    silent = TRUE)
-  !inherits(z, "try-error")
-}
-
-skip_if_net_down <- function() {
-  if (has_internet()) {
-    return()
-  }
-  testthat::skip("no internet")
-}
