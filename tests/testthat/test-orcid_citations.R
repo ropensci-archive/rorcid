f934b5f5f2943f882697c6bf28ff233afec2a4a0
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-07-30 20:27:01 +0200

    Remove whitespace and not needed lines

--- tests/testthat/test-orcid_citations.R
+++ tests/testthat/test-orcid_citations.R
@@ -1,12 +1,10 @@
-test_that("orcid_citations basic test", {
+test_that("orcid_citations_basic_test", {
   skip_on_cran()
 
   ##this tests a specific ORCID record with a bibtex record to extract
-  vcr::use_cassette("orcid_citations with bibtex", {
+  vcr::use_cassette("orcid_citations_with_bibtex", {
     aa <- orcid_citations(orcid = "0000-0002-0734-2199", put_code = "40038622")
-
   })
 
   expect_is(aa, "data.frame")
-
 })
