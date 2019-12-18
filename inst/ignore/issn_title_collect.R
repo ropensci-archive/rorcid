cb27b1879bfeefc4d1c4786ac7e828c0567542e3
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-18 09:24:04 -0800

    fix non-ascii characters in issn_title dataset

--- inst/ignore/issn_title_collect.R
+++ inst/ignore/issn_title_collect.R
@@ -21,3 +21,18 @@ dat <- dat[!is.na(names(dat))]
 # save to data/issn_title.rda
 issn_title <- dat
 save(issn_title, file = "data/issn_title.rda", compress = "xz")
+
+
+# Cleanup non-ASCII characters
+
+# issn_title_badascii = lapply(unname(issn_title), tools::showNonASCII)
+# non_asciis <- which(vapply(issn_title_badascii, function(z) length(z) == 1, logical(1)))
+# issn_title[[non_asciis[1]]]
+
+# for (i in seq_along(issn_title)) {
+#   if (i %in% non_asciis) {
+#     issn_title[[i]] <- stringi::stri_escape_unicode(issn_title[[i]])
+#   }
+# }
+# issn_title2 <- issn_title
+# save(issn_title, file = "data/issn_title.rda", compress = "xz")
