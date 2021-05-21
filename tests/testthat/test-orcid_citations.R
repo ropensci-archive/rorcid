eb53849c66bd59c39eff5925285476df6f0aa324
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-05-21 22:25:26 +0100

    Fix last line problem and fix test, which did not work anymore because the put code was invalid

--- tests/testthat/test-orcid_citations.R
+++ tests/testthat/test-orcid_citations.R
@@ -3,7 +3,7 @@ test_that("orcid_citations_basic_test", {
 
   ##this tests a specific ORCID record with a bibtex record to extract
   vcr::use_cassette("orcid_citations_with_bibtex", {
-    aa <- orcid_citations(orcid = "0000-0002-0734-2199", put_code = "40038622")
+    aa <- orcid_citations(orcid = "0000-0002-0734-2199", put_code = "77226586")
   })
 
   expect_is(aa, "data.frame")
